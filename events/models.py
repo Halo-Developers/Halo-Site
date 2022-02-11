0e0fc1f2d1f77b7e7938fb9c3b0ee76345fb2f1a
kuyesu <2019bce028@std.must.ac.ug>
2022-02-11 15:12:27 +0000

    update requirements and the models for events

--- events/models.py
+++ events/models.py
@@ -1,29 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+"""Imported models"""
+from mapbox_location_field.models import LocationField
+from ckeditor_uploader.fields import RichTextUploadingField
+
 # Create your models here.
-
-""""The models containing the data for the events app."""
-class Event(models.Model):
-    """
-    The Event model.
-    """
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    location = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='events/images')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        """
-        Return the name of the event.
-        """
-        return self.name
-
 
 
 """Event Category Models are literally defined here"""
@@ -51,5 +33,52 @@ class EventCategory(models.Model):
     def get_absolute_url(self):
         return reverse("event-category-list", kwargs={"pk": self.pk})
 
-        
+
     
+""""The models containing the data for the events app."""
+class Event(models.Model):
+    """
+    The Event model.
+    """
+    category = models.ForeignKey(EventCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    uid = models.PositiveIntegerField(unique=True)
+    description = RichTextUploadingField()
+    job_category = models.ForeignKey(JobCategory, on_delete=models.CASCADE)
+    select_scheduled_status = (
+        ('yet to scheduled', 'Yet to Scheduled'),
+        ('scheduled', 'Scheduled')
+    )
+    scheduled_status = models.CharField(max_length=25, choices=select_scheduled_status)
+    venue = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    location = LocationField()
+    points = models.PositiveIntegerField()
+    maximum_attende = models.PositiveIntegerField()
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True, related_name='event_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True, related_name='event_updated_user')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status_choice = (
+        ('disabled', 'Disabled'),
+        ('active', 'Active'),
+        ('deleted', 'Deleted'),
+        ('time out', 'Time Out'),
+        ('completed', 'Completed'),
+        ('cancel', 'Cancel'),
+    )
+    status = models.CharField(choices=status_choice, max_length=10)
+
+    def __str__(self):
+        """
+        Return the name of the event.
+        """
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('event-list')
+    
+  
+
