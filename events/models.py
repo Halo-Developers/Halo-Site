033b9a09c98f50d818710080575a542b856938d0
kuyesu <2019bce028@std.must.ac.ug>
2022-02-12 08:03:16 +0000

    defining events refined

--- events/models.py
+++ events/models.py
@@ -34,6 +34,12 @@ class EventCategory(models.Model):
         return reverse("event-category-list", kwargs={"pk": self.pk})
 
 
+class JobCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
     
 """"The models containing the data for the events app."""
 class Event(models.Model):
@@ -80,5 +86,132 @@ class Event(models.Model):
     def get_absolute_url(self):
         return reverse('event-list')
     
-  
+    def created_updated(model, request):
+        obj = model.objects.latest('pk')
+        if obj.created_by is None:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
 
+class EventImage(models.Model):
+    event = models.OneToOneField(Event, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='event_image/')
+
+    """Return the name of the event"""
+    def __str__(self):
+        return self.event.name
+class EventAgenda(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    session_name = models.CharField(max_length=120)
+    speaker_name = models.CharField(max_length=120)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    venue_name = models.CharField(max_length=255)
+
+    """Retur the event agenda session"""
+    def __str__(self):
+        return self.session_name
+
+class EventJobCategoryLinking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    job_category = models.ForeignKey(JobCategory, on_delete=models.CASCADE)
+    status_choice = (
+        ('disabled', 'Disabled'),
+        ('active', 'Active'),
+        ('deleted', 'Deleted'),
+        ('blocked', 'Blocked'),
+        ('completed', 'Completed'),
+    )
+    status = models.CharField(choices=status_choice, max_length=10)
+
+    def __str__(self):
+        return str(self.event)
+
+
+class EventMember(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    attend_status_choice = (
+        ('waiting', 'Waiting'),
+        ('attending', 'Attending'),
+        ('completed', 'Completed'),
+        ('absent', 'Absent'),
+        ('cancelled', 'Cancelled'),
+    )
+    attend_status = models.CharField(choices=attend_status_choice, max_length=10)
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventmember_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventmember_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status_choice = (
+        ('disabled', 'Disabled'),
+        ('active', 'Active'),
+        ('deleted', 'Deleted'),
+        ('blocked', 'Blocked'),
+        ('completed', 'Completed'),
+    )
+    status = models.CharField(choices=status_choice, max_length=10)
+
+
+    class Meta:
+        unique_together = ['event', 'user']
+
+    def __str__(self):
+        return str(self.user)
+    
+    def get_absolute_url(self):
+        return reverse('join-event-list')
+
+class EventUserWishList(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventwishlist_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventwishlist_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status_choice = (
+        ('disabled', 'Disabled'),
+        ('active', 'Active'),
+        ('deleted', 'Deleted'),
+        ('blocked', 'Blocked'),
+        ('completed', 'Completed'),
+    )
+    status = models.CharField(choices=status_choice, max_length=10)
+
+
+    class Meta:
+        unique_together = ['event', 'user']
+
+    def __str__(self):
+        return str(self.event)
+    
+    def get_absolute_url(self):
+        return reverse('event-wish-list')
+
+
+class UserCoin(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    CHOICE_GAIN_TYPE = (
+        ('event', 'Event'),
+        ('others', 'Others'),
+    )
+    gain_type = models.CharField(max_length=6, choices=CHOICE_GAIN_TYPE)
+    gain_coin = models.PositiveIntegerField()
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='usercoin_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='usercoin_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status_choice = (
+        ('disabled', 'Disabled'),
+        ('active', 'Active'),
+        ('deleted', 'Deleted'),
+        ('blocked', 'Blocked'),
+        ('completed', 'Completed'),
+    )
+    status = models.CharField(choices=status_choice, max_length=10)
+
+    def __str__(self):
+        return str(self.user)
+    
+    def get_absolute_url(self):
+        return reverse('user-mark')
